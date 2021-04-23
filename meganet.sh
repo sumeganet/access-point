1e22c587a686b928e673ce7e0160ad2273930ed4
khoahoc <79499925+khoahoc@users.noreply.github.com>
2021-04-23 18:23:08 +0700

    Update meganet.sh

--- meganet.sh
+++ meganet.sh
@@ -1,1 +1,255 @@
-first!
+#!/bin/sh
+# wget --no-check-certificate  https://github.com/sumeganet/access-point/edit/main/meganet.sh -O - -q | sh
+
+###############################################################################################################################
+###                                             MEGANET SYSTEM INFO
+###############################################################################################################################
+
+# Lay MAC address cua thiet bi hien tai
+getCurrentAPMacAddress(){
+	## Mac should be in br-lan or br-wan
+	local MAC_BRLAN=`ifconfig | grep br-lan | awk '{print $5}'` 
+	local MAC_BRWAN=`ifconfig | grep br-wan | awk '{print $5}'` 
+
+	if [ "${#MAC_BRLAN}" -gt 1 ]                                       
+	then                                                   
+	    MAC=$MAC_BRLAN
+	elif [ "${#MAC_BRWAN}" -gt 1 ]
+	then
+		MAC=$MAC_BRWAN
+	else
+		echo "Not detected MAC: $MAC on this model! Please contact sysadmin@meganet.com.vn for support this device." >&2; exit 1
+	fi
+}
+
+# Lay MEGANET ID tren Demo MegaNet
+getMeganetID()
+{
+	MEGANET_AP_ID=`wget --no-check-certificate -O - -q "https://demo.meganet.com.vn/api/Prometheus/getDevice?field=ap_number&device=$MAC"`
+
+	if  [ -z "$MEGANET_AP_ID" ] ; then
+	  printf "Not found this device on Meganet Portal. \n Please contact sysadmin@meganet.com.vn for free support." >&2; exit 1
+	fi	
+}
+
+# Lay Local IP tren Demo MegaNet
+getLocalNatIP()
+{
+	LOCAL_NAT_IP=`wget --no-check-certificate -O - -q "https://demo.meganet.com.vn/api/Prometheus/getDevice?field=ip_local&device=$MAC" | tr -d "\""`
+	if  [ -z "${#LOCAL_NAT_IP}" ]
+	then
+	  echo "Cannot generate ip for connecting MegaNet Portal. Please contact sysadmin@meganet.com.vn for free support." >&2; exit 1
+	fi
+}
+
+
+# Bat dau qua trinh cai dat
+## Khoi tao gia tri
+getCurrentAPMacAddress
+getMeganetID
+getLocalNatIP
+
+## Print thong tin he thong
+printf "\n
+---------====[ MEGANET INSTALLATION ACCESS POINT PROGRAM ] ====---------
+
+Your Mac Address: $MAC
+Your Meganet ID: $MEGANET_AP_ID 
+Your Local NAT IP: $LOCAL_NAT_IP \n"
+
+
+###############################################################################################################################
+###                                             AUTO SSSH
+###############################################################################################################################
+
+generateNewAutoSSHConfig()
+{
+    echo "Being generate new AutoSSH config file and put to /tmp/autossh"
+    echo "config autossh
+	option ssh	'-N -T
+				-o StrictHostKeyChecking=no
+				-o ServerAliveInterval=60
+				-o ServerAliveCountMax=10
+				-R $LOCAL_NAT_IP:2222:localhost:22 
+				-R $LOCAL_NAT_IP:9100:localhost:9100
+				-R $LOCAL_NAT_IP:7681:localhost:7681
+                -i /etc/dropbear/id_rsa
+				 noaccess@prometheus.meganet.com.vn'
+	option gatetime	'0'
+	option monitorport	'0'
+	option poll	'600'
+	option enabled	'1'" > /tmp/autossh
+}
+
+compareAutoSSHConfig()
+{
+    generateNewAutoSSHConfig
+    currentAutoSSHConfig = `sha256sum /etc/config/autossh  | awk '{print $1}'`
+    newAutoSSHConfig = `sha256sum /tmp/autossh  | awk '{print $1}'`
+    if [ $currentAutoSSHConfig -eq $newAutoSSHConfig ]
+    then
+        echo 'AutoSSH Config matched\n'
+    else
+        echo 'Update new AutoSSH Config\n'
+        mv /tmp/autossh /etc/config/autossh
+    fi
+}
+
+# Dam bao rang khi reboot, dich vu duoc khoi dong theo
+/etc/init.d/autossh enable
+
+# Setup AutoSSH
+if [ -f "/etc/config/autossh" ]; 
+then
+    generateNewAutoSSHConfig
+    mv /tmp/autossh/ /etc/config/
+else 
+    compareAutoSSHConfig
+fi
+
+
+# Kiem tra AutoSSH da chay chua?
+autoSSH_STATUS=`ps  | grep autossh | wc -l`
+if [ $autoSSH_STATUS -gt 1 ]
+then
+    printf "AutoSSH: OK\n"
+else 
+    /etc/init.d/autossh restart
+    printf "AutoSSH: Restarted\n"
+fi
+
+###############################################################################################################################
+###                                             NODE EXPORTER
+###############################################################################################################################
+
+# Dam bao rang khi reboot, dich vu duoc khoi dong theo
+/etc/init.d/prometheus-node-exporter-lua enable
+
+# Kiem tra Node Exporter da chay chua?
+nodeExporter_STATUS=`netstat -nltp | grep 9100 | wc -l`
+if [ $nodeExporter_STATUS -gt 0 ]
+then
+    printf "Node Exporter: OK\n"
+else 
+    /etc/init.d/prometheus-node-exporter-lua restart
+    printf "Node Exporter: Restarted\n"
+fi
+
+###############################################################################################################################
+###                                             PROMETHEUS SERVER
+###############################################################################################################################
+
+# Kiem tra phan mem giam sat da chay chua?
+prometheusServer_STATUS=`wget --no-check-certificate -O - -q "https://demo.meganet.com.vn/api/Prometheus/getUpExporter?device=$LOCAL_NAT_IP"`
+if [ $STATUS -eq 0 ]
+then
+    /etc/init.d/prometheus-node-exporter-lua restart
+    /etc/init.d/autossh restart
+    printf "Node Exporter & AutoSSH: Restarted\n"
+else
+    printf "Prometheus Server: OK\n"
+fi
+
+
+###############################################################################################################################
+###                                             TTYD
+###############################################################################################################################
+
+# Dam bao rang khi reboot, dich vu duoc khoi dong theo
+/etc/init.d/ttyd enable
+
+generateNewTTYDConfig()
+{
+    echo "Being generate new TTYD config file and put to /tmp/autossh"
+    echo "config ttyd
+	option interface '@loopback'
+	option command '/bin/sh -l'
+" > /tmp/ttyd
+}
+
+compareTTYDConfig()
+{
+    generateNewTTYDConfig
+    currentTTYDConfig = `sha256sum /etc/config/ttyd  | awk '{print $1}'`
+    newTTYDConfig = `sha256sum /tmp/ttyd  | awk '{print $1}'`
+    if [ $currentTTYDConfig -eq $newTTYDConfig ]
+    then
+        echo 'TTYD Config matched\n'
+    else
+        echo 'Update new TTYD Config\n'
+        mv /tmp/ttyd /etc/config/ttyd
+    fi
+}
+
+# Setup TTYD
+if [ -f "/etc/config/ttyd" ]; 
+then
+    generateNewTTYDConfig
+    mv /tmp/autossh/ /etc/config/
+else 
+    compareTTYDConfig
+fi
+
+# Kiem tra TTYD da chay chua?
+TTYD_STATUS=`netstat -nltp  | grep 7681 | wc -l `
+if [ $TTYD_STATUS -gt 0 ]
+then
+    printf "TTYD: OK\n"
+else 
+    /etc/init.d/ttyd restart
+    printf "TTYD: Restarted\n"
+fi
+
+###############################################################################################################################
+###                                             CRONTAB
+###############################################################################################################################
+
+# Dam bao rang khi reboot, dich vu duoc khoi dong theo
+/etc/init.d/cron enable
+
+# Kiem tra Crontab da chay chua?
+cron_STATUS=`netstat -nltp  | grep crond | wc -l `
+if [ $cron_STATUS -gt 0 ]
+then
+    printf "Crontabs: OK"
+else
+    /etc/init.d/cron restart
+    printf "Crontabs: Restarted\n"
+fi
+
+
+###############################################################################################################################
+###                                             MEGANET SCRIPT
+###############################################################################################################################
+
+generateNewMeganetScript()
+{
+    echo "Being download new crontab file and put to /tmp/meganet.sh"
+    wget --no-check-certificate https://raw.githubusercontent.com/sumeganet/access-point/main/meganet.sh -O /tmp/meganet.sh
+}
+
+compareMeganetScript()
+{
+    generateNewMeganetScript
+    currentMeganetScript = `sha256sum /www/meganet.sh  | awk '{print $1}'`
+    newMeganetScript = `sha256sum /tmp/meganet.sh  | awk '{print $1}'`
+    if [ $currentMeganetScript -eq $newMeganetScript ]
+    then
+        echo 'Meganet Script matched\n'
+    else
+        echo 'Update new Meganet Script\n'
+        mv /tmp/ttyd /etc/config/ttyd
+    fi
+}
+
+# Setup Meganet Script
+if [ -f "/www/meganet.sh" ]; 
+then
+    generateNewMeganetScript
+    mv /tmp/autossh/ /etc/config/
+else 
+    compareMeganetScript
+fi
+
+printf "=========== INSTALLATION COMPLETED! ============\n"
+printf "Note: Please reboot AP to make sure everything working exacely!!\n"
